0aa8cf4dfbcb2864b35aa66f0aa236cb687d4d1b
Shrirang Dharkar <dharkarshrirang@gmail.com>
2023-08-02 21:34:10 +0530

    Committing the jwt-auth mechanism creation in progress

--- UI/CafeManagement/src/app/login/login.component.ts
+++ UI/CafeManagement/src/app/login/login.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../service/user.service';
+import { Router } from '@angular/router';
+import { User } from 'src/model/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  users : User[] = [];
+  user : User = new User();
+
+  constructor(private userService : UserService, private router: Router){}
+
+  ngOnInit(): void {
+      
+  }
+
+  onSubmit(){
+    console.log('Login successfully!!');
+  }
 
 }
